55e4c715a4fafa63fbec85485d47415ba27f063e
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-04-05 11:27:48 +0600

    about added

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./About.css"
 
 const About = () => {
     return (
         <div>
-            <h2>About</h2>
+            <h1>ABOUT ASUS</h1>
+            <p className='text-contant'>ASUSTeK COMPUTER INC. and its affiliated entities companies use cookies and similar technologies to perform essential online functions, such as authentication and security. You may disable these by changing your cookies setting through browser, but this may affect how this website functions.
+                Also, ASUS uses some analytics, targeting, adverting and video-embedded cookies provided by ASUS or third parties. Please click a button here to choose your preference for these types of cookies. Please note that you can configure cookies settings by accessing the browser you install at any time. For detailed information, please visit ASUS Privacy Policy- “Cookies and similar technologies”.</p>
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <div className="text-container">
                     <p className='large color'>Your next laptop</p>
                     <p className='large'>Your best laptop</p>
-                    <p className='text-contant'>ASUS ExpertBook L1 L1500CDA Laptop boasts high performance, military-grade durability, and extensive customization. This new ASUS ExpertBook L1 L1500CDA comes with AMD Ryzen 3 3250U Processor (4M Cache, 2.1 GHz up to 3.7 GHz) and AMD Radeon Graphics. Its also engineered to confidently handle whatever tasks come your way, with a full set of I/O ports, speedy WiFi 6, and cutting-edge AI noise-canceling technology. ExpertBook L1 streamlines your workflow to keep you productive.</p>
+                    <p className='text-contant'>ASUS is passionate about technology and driven by innovation. We dream, we dare and we strive to create an effortless and joyful digital life for everyone. We're always in search of incredible ideas and experiences, and we aspire to deliver the incredible in everything we do.</p>
                 </div>
                 <div className="picture-container">
                     <img src={image} alt="arif" />
@@ -17,6 +17,7 @@ const Home = () => {
             </div>
             <div>
                 <h1>Customer Reviews (3)</h1>
+
             </div>
         </div>
 
