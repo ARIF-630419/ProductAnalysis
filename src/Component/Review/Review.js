6968fbac7a96fb03b6e08726884989d08464b0b5
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-04-05 19:43:05 +0600

    reviews added

---
+++ src/Component/Review/Review.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Review.css";
+const Review = (props) => {
+    const { name, text, rating } = props.review;
+    return (
+        <div>
+            <h3>Name : {name}</h3>
+            <p><span>Text :</span> {text}</p>
+            <p><small><span>Rating :</span> {rating}</small></p>
+        </div>
+    );
+};
+
+export default Review;
--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
+import "./Reviews.css"
 
 const Reviews = () => {
+    const [reviews, setReviews] = useReviews();
     return (
-        <div>
-            <h2>Reviews</h2>
+        <div className='reviews-container'>
+            {
+                reviews.map(review => <Review
+                    key={review.id}
+                    review={review}
+                ></Review>)
+            }
         </div>
     );
 };
